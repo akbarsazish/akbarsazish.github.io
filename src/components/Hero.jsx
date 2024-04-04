7565c3417fd70c558a2dfe3bfc7dc269da9831ff
akbarsazish <sazishaliakbar@gmail.com>
2024-04-04 23:35:14 +0330

    add section wrapper in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,7 @@ const Hero = () => {
   
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[100px] mx-w-7xl 
-        mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[100px] max-w-7xl mx-auto flex flex-row items-start`}>
         <div className="flex flex-col items-center justify-center mt-5">
            <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
            <div className="w-1 sm:h-90 h-80 violet-gradient" />
@@ -66,4 +65,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default SectionWrapper(Hero, "");
