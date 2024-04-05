7ddee3372e21b9094975ea6663f30c87e8a9de2c
akbarsazish <sazishaliakbar@gmail.com>
2024-04-05 09:16:58 +0330

    removed section wrapper

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,9 @@ const Hero = () => {
 
   
   return (
+   
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[100px] flex flex-row items-start`}>
+      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className="flex flex-col items-center justify-center mt-5">
            <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
            <div className="w-1 sm:h-90 h-80 violet-gradient" />
@@ -66,4 +67,4 @@ const Hero = () => {
   )
 }
 
-export default SectionWrapper(Hero, "");
+export default Hero;
