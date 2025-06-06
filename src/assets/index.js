1facb94d9263dd3cfc57e9b3d5ee2e47d36472cf
akbarsazish <sazishaliakbar@gmail.com>
2025-06-06 21:25:16 +0330

    Add update github action

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,11 +13,6 @@ import microvers from "./company/microvers.png";
 import likeCard from "./likeCard.png";
 import starfood from "./starfood.png";
 import crm from "./crm.png";
-import math from "./math.png";
-import air_pollution from "./air_pollution.png";
-import awesome_books from "./awesome_books.png";
-import book_store from "./book_store.png";
-import online_academy from "./online_academy.png";
 import accounting from "./accounting.jfif";
 import sattagydia from "./sattagydia.png";
 
@@ -35,11 +30,6 @@ export {
   microvers,
   starfood,
   crm,
-  math,
-  air_pollution,
-  awesome_books,
-  book_store,
-  online_academy,
   accounting,
   sattagydia
 };
